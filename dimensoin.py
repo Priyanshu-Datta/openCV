eb78b0031a7ec0e7b3d5a1c72a697313a71f6b67
Priyanshu-Datta <priyanshudatta80@gmail.com>
2026-02-25 02:29:38 +0530

    shape

---
+++ dimensoin.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread('test.JPG')
+
+if image is None:
+    print('Could not read the image.')
+else:
+    print('Image read successfully.')
+    h,m,c = image.shape
+    print('Height:', h)
+    print('Width:', m)
+    print('Channels:', c)
