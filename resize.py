de8a86f75029f4a4f9ce49d7ab59d192d144a451
Priyanshu-Datta <priyanshudatta80@gmail.com>
2026-03-13 01:26:05 +0530

    resizing

---
+++ resize.py
@@ -0,0 +1,16 @@
+import cv2
+img = cv2.imread('test.jpg')
+
+if img is None:
+    print('Could not open or find the image.')
+else:
+    print("image is loaded successfully")
+    print('Original Dimensions : ', img.shape)
+    resized = cv2.resize(img, (300, 300)) 
+    print('Resized Dimensions : ', resized.shape)
+    
+    choice = input("do you want to see the image? (y/n): ")
+    if choice.lower() == 'y':
+        cv2.imshow('Resized Image', resized)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
